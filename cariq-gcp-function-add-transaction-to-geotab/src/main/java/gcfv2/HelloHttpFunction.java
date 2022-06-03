bc82fb452866fcf7afeef3e60dca8743a19b44b3
shaival <shaival12@gmail.com>
2022-06-02 22:36:40 -0700

    added add transaction to geotab function

---
+++ cariq-gcp-function-add-transaction-to-geotab/src/main/java/gcfv2/HelloHttpFunction.java
@@ -0,0 +1,22 @@
+package gcfv2;
+
+import java.io.BufferedWriter;
+import java.util.logging.Logger;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+public class HelloHttpFunction implements HttpFunction {
+	
+	 private static final Logger logger = Logger.getLogger(HelloHttpFunction.class.getName());
+	 
+ public void service(final HttpRequest request, final HttpResponse response) throws Exception {
+	 
+	 logger.info("I am an info log at : [ " + System.currentTimeMillis()+ " ]" );
+	 logger.warning("I am a warning log!");
+	    
+     final BufferedWriter writer = response.getWriter();
+      writer.write("Hello world calling from GCP function");
+ }
+}
