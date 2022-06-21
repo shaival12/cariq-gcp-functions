e4fcc4baf355e6d0065bb8648d53a4272feb8c5a
shaival <shaival12@gmail.com>
2022-06-21 14:38:34 -0700

    removed pass

--- cariq-gcp-function-add-transaction-to-geotab/src/main/java/gcfv2/Constants.java
+++ cariq-gcp-function-add-transaction-to-geotab/src/main/java/gcfv2/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
 	//Prod 
 	static final Object DB_NAME = "jdbc:postgresql:///cariq_prod";
 	static final String DB_USER = "shaival";
-	static final String DB_PASS = "Mf6GGYBx95sJt8nL";
+	static final String DB_PASS = "";
 	static final String INSTANCE_CONNECTION_NAME = "cariq-vehicle-data-staging:us-central1:vehicle-data";
 
 }
